710db1ee263b100f0f888c8a510069d0cbdf067e
AlexDooo <krash_pvp@mail.ru>
2021-02-12 17:30:04 +0300

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat bean2 = (Cat) applicationContext.getBean("Cat");
+        System.out.println(bean2.getMessage());
+
+        Cat bean3 = (Cat) applicationContext.getBean("Cat");
+        System.out.println(bean3.getMessage());
+
     }
 }
